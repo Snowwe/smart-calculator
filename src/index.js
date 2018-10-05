32eecc0dfe81af757676a9a6a2ecd26d969533ca
Snezhana <snowwe25@rambler.ru>
2018-10-06 02:37:27 +0300

    version_1 301/301

--- src/index.js
+++ src/index.js
@@ -1,27 +1,79 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.colculationArray = [initialValue];
+        this.operands = ['^', '*', '/', '-', '+'];
+    }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    add(number) {
+        this.colculationArray.push('+', number);
+        return this;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    subtract(number) {
+        this.colculationArray.push('-', number);
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    multiply(number) {
+        this.colculationArray.push('*', number);
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    devide(number) {
+        this.colculationArray.push('/', number);
+        return this;
+    }
+
+    pow(number) {
+        this.colculationArray.push('^', number);
+        return this;
+    }
+
+    toString() {
+        let tempResult;
+        for (let i = 0, lenO = this.operands.length; i < lenO; ++i) {
+            let operand = this.operands[i];
+
+            for (let len = this.colculationArray.length, j = len; j >= 0; --j) {
+                let valueFromArray = this.colculationArray[j];
+
+                if (valueFromArray === '^') {
+                    tempResult = Math.pow(this.colculationArray[j - 1], this.colculationArray[j + 1]);
+                    this.colculationArray.splice(j - 1, 3, tempResult);
+                    j = len;
+                } else continue;
+            }
+
+            for (let j = 0, len = this.colculationArray.length; j < len; ++j) {
+                let valueFromArray = this.colculationArray[j];
+
+                if (valueFromArray === operand) {
+                    if (valueFromArray === '*') {
+                        tempResult = this.colculationArray[j - 1] * this.colculationArray[j + 1];
+                        this.colculationArray.splice(j - 1, 3, tempResult);
+                        j = 0;
+                    }
+                    else if (valueFromArray === '/') {
+                        tempResult = this.colculationArray[j - 1] / this.colculationArray[j + 1];
+                        this.colculationArray.splice(j - 1, 3, tempResult);
+                        j = 0;
+                    }
+                    else if (valueFromArray === '+') {
+                        tempResult = this.colculationArray[j - 1] + this.colculationArray[j + 1];
+                        this.colculationArray.splice(j - 1, 3, tempResult);
+                        j = 0;
+                    }
+                    else if (valueFromArray === '-') {
+                        tempResult = this.colculationArray[j - 1] - this.colculationArray[j + 1];
+                        this.colculationArray.splice(j - 1, 3, tempResult);
+                        j = 0;
+                    }
+                    else continue;
+                }
+            }
+        }
+        return this.colculationArray[0];
+    }
 }
 
 module.exports = SmartCalculator;
