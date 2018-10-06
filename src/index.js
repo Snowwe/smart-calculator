3ba212f0e72257b843c6aa8e1e58750e4d88bbd2
Snezhana <snowwe25@rambler.ru>
2018-10-06 17:52:07 +0300

    version_3.1 301/301

--- src/index.js
+++ src/index.js
@@ -1,82 +1,82 @@
 class SmartCalculator {
     constructor(initialValue) {
-        this.colculationArray = [initialValue];
+        this.calculationArray = [initialValue];
         this.operands = ['^', '*', '/', '-', '+'];
     }
 
     add(number) {
-        this.colculationArray.push('+', number);
+        this.calculationArray.push('+', number);
         return this;
     }
 
     subtract(number) {
-        this.colculationArray.push('-', number);
+        this.calculationArray.push('-', number);
         return this;
     }
 
     multiply(number) {
-        this.colculationArray.push('*', number);
+        this.calculationArray.push('*', number);
         return this;
     }
 
     devide(number) {
-        this.colculationArray.push('/', number);
+        this.calculationArray.push('/', number);
         return this;
     }
 
     pow(number) {
-        this.colculationArray.push('^', number);
+        this.calculationArray.push('^', number);
         return this;
     }
 
     toString() {
-        return this.calculate(this.colculationArray);
+        return this.calculate(this.calculationArray);
     }
 
-    calculate(colculationArray) {
+    calculate(calculationArray) {
         let tempResult;
         for (let i = 0, lenO = this.operands.length; i < lenO; ++i) {
             let operand = this.operands[i];
 
-            for (let len = colculationArray.length, j = len; j >= 0; --j) {
-                let valueFromArray = colculationArray[j];
+            for (let len = calculationArray.length, j = len; j >= 0; --j) {
+                let valueFromArray = calculationArray[j];
 
                 if (valueFromArray === '^') {
-                    tempResult = Math.pow(colculationArray[j - 1], colculationArray[j + 1]);
-                    colculationArray.splice(j - 1, 3, tempResult);
+                    tempResult = Math.pow(calculationArray[j - 1], calculationArray[j + 1]);
+                    calculationArray.splice(j - 1, 3, tempResult);
                     j = len;
                 } else continue;
             }
 
-            for (let j = 0, len = colculationArray.length; j < len; ++j) {
-                let valueFromArray = colculationArray[j];
+            for (let j = 0, len = calculationArray.length; j < len; ++j) {
+                let valueFromArray = calculationArray[j];
 
                 if (valueFromArray === operand) {
                     if (valueFromArray === '*') {
-                        tempResult = colculationArray[j - 1] * colculationArray[j + 1];
-                        colculationArray.splice(j - 1, 3, tempResult);
+                        tempResult = calculationArray[j - 1] * calculationArray[j + 1];
+                        calculationArray.splice(j - 1, 3, tempResult);
                         j = 0;
                     }
                     else if (valueFromArray === '/') {
-                        tempResult = colculationArray[j - 1] / colculationArray[j + 1];
-                        colculationArray.splice(j - 1, 3, tempResult);
+                        tempResult = calculationArray[j - 1] / calculationArray[j + 1];
+                        calculationArray.splice(j - 1, 3, tempResult);
                         j = 0;
                     }
                     else if (valueFromArray === '+') {
-                        tempResult = colculationArray[j - 1] + colculationArray[j + 1];
-                        colculationArray.splice(j - 1, 3, tempResult);
+                        tempResult = calculationArray[j - 1] + calculationArray[j + 1];
+                        calculationArray.splice(j - 1, 3, tempResult);
                         j = 0;
                     }
                     else if (valueFromArray === '-') {
-                        tempResult = colculationArray[j - 1] - colculationArray[j + 1];
-                        colculationArray.splice(j - 1, 3, tempResult);
+                        tempResult = calculationArray[j - 1] - calculationArray[j + 1];
+                        calculationArray.splice(j - 1, 3, tempResult);
                         j = 0;
                     }
                     else continue;
                 }
             }
         }
-        return colculationArray[0];
+        return calculationArray[0];
     }
 }
 
