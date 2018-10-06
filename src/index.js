afa00b58a31cf36485bf83a42e0c1ff9c771d88d
Snezhana <snowwe25@rambler.ru>
2018-10-06 17:47:30 +0300

    version_3 301/301

--- src/index.js
+++ src/index.js
@@ -30,53 +30,53 @@ class SmartCalculator {
     }
 
     toString() {
-        return this.calculate();
+        return this.calculate(this.colculationArray);
     }
 
-    calculate() {
+    calculate(colculationArray) {
         let tempResult;
         for (let i = 0, lenO = this.operands.length; i < lenO; ++i) {
             let operand = this.operands[i];
 
-            for (let len = this.colculationArray.length, j = len; j >= 0; --j) {
-                let valueFromArray = this.colculationArray[j];
+            for (let len = colculationArray.length, j = len; j >= 0; --j) {
+                let valueFromArray = colculationArray[j];
 
                 if (valueFromArray === '^') {
-                    tempResult = Math.pow(this.colculationArray[j - 1], this.colculationArray[j + 1]);
-                    this.colculationArray.splice(j - 1, 3, tempResult);
+                    tempResult = Math.pow(colculationArray[j - 1], colculationArray[j + 1]);
+                    colculationArray.splice(j - 1, 3, tempResult);
                     j = len;
                 } else continue;
             }
 
-            for (let j = 0, len = this.colculationArray.length; j < len; ++j) {
-                let valueFromArray = this.colculationArray[j];
+            for (let j = 0, len = colculationArray.length; j < len; ++j) {
+                let valueFromArray = colculationArray[j];
 
                 if (valueFromArray === operand) {
                     if (valueFromArray === '*') {
-                        tempResult = this.colculationArray[j - 1] * this.colculationArray[j + 1];
-                        this.colculationArray.splice(j - 1, 3, tempResult);
+                        tempResult = colculationArray[j - 1] * colculationArray[j + 1];
+                        colculationArray.splice(j - 1, 3, tempResult);
                         j = 0;
                     }
                     else if (valueFromArray === '/') {
-                        tempResult = this.colculationArray[j - 1] / this.colculationArray[j + 1];
-                        this.colculationArray.splice(j - 1, 3, tempResult);
+                        tempResult = colculationArray[j - 1] / colculationArray[j + 1];
+                        colculationArray.splice(j - 1, 3, tempResult);
                         j = 0;
                     }
                     else if (valueFromArray === '+') {
-                        tempResult = this.colculationArray[j - 1] + this.colculationArray[j + 1];
-                        this.colculationArray.splice(j - 1, 3, tempResult);
+                        tempResult = colculationArray[j - 1] + colculationArray[j + 1];
+                        colculationArray.splice(j - 1, 3, tempResult);
                         j = 0;
                     }
                     else if (valueFromArray === '-') {
-                        tempResult = this.colculationArray[j - 1] - this.colculationArray[j + 1];
-                        this.colculationArray.splice(j - 1, 3, tempResult);
+                        tempResult = colculationArray[j - 1] - colculationArray[j + 1];
+                        colculationArray.splice(j - 1, 3, tempResult);
                         j = 0;
                     }
                     else continue;
                 }
             }
         }
-        return this.colculationArray[0];
+        return colculationArray[0];
     }
 }
 
